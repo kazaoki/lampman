6689dec7151771e967ea286b4a1ebcf705d5debc
Tatsuya Karino <karino@kazaoki.jp>
2019-10-18 22:47:27 +0900

    [UPDATE] UP時にshファイルに実行権を自動的に付加するように

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
 <?php
-file_put_contents('app.log', date('Y-m-d H:i:s')."\n", FILE_APPEND);
-error_log('error-log-test');
+// file_put_contents('app.log', date('Y-m-d H:i:s')."\n", FILE_APPEND);
+// error_log('error-log-test');
 
 $data = [
     'A1' => [
