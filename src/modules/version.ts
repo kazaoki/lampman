5442cec0ef10189907f3fb79c54726740be9cea6
karino <karino@kazaoki.jp>
2019-10-26 11:45:18 +0900

    [ADD] version: -qオプション追加

--- src/modules/version.ts
+++ src/modules/version.ts
@@ -11,6 +11,7 @@
 declare let lampman:any;
 
 import libs = require('../libs');
+const color = require('cli-color');
 
 /**
  * コマンド登録用メタデータ
@@ -20,6 +21,9 @@ export function meta()
     return {
         command: 'version',
         description: 'バージョン表示',
+        options: [
+            ['-q, --quit', 'バージョン文字列のみ出力する'],
+        ],
     }
 }
 
@@ -28,10 +32,15 @@ export function meta()
  */
 export function action(commands:any)
 {
-    libs.Message(
-        `Lampman ver ${libs.getLampmanVersion()}\n`+
-        `mode: ${lampman.mode}`,
-        'primary',
-        1
-    )
+    if(commands.quit) {
+        process.stdout.write(color.move(0, -1))
+        process.stdout.write(libs.getLampmanVersion())
+    }  else {
+        libs.Message(
+            `Lampman ver ${libs.getLampmanVersion()}\n`+
+            `mode: ${lampman.mode}`,
+            'primary',
+            1
+        )
+    }
 }
