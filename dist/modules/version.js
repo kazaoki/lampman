5ac11d2875b3ea3faebbcdd5a0a5cf66da66ad79
karino <karino@kazaoki.jp>
2019-11-22 22:43:08 +0900

    [UPDATE] yargs対応: version実装

--- dist/modules/version.js
+++ dist/modules/version.js
@@ -4,16 +4,20 @@ var libs = require("../libs");
 var color = require('cli-color');
 function meta() {
     return {
-        command: 'version',
-        description: 'バージョン表示',
-        options: [
-            ['-q, --quiet', 'バージョン文字列のみ出力する'],
-        ],
+        command: 'version [options]',
+        describe: 'バージョン表示',
+        options: {
+            'quiet': {
+                alias: 'q',
+                describe: 'バージョン文字列のみ出力します。',
+                type: 'boolean',
+            },
+        },
     };
 }
 exports.meta = meta;
-function action(commands) {
-    if (commands.quiet) {
+function action(argv, lampman) {
+    if (argv.quiet) {
         process.stdout.write(color.move(0, -1));
         process.stdout.write(libs.getLampmanVersion());
     }
--- src/modules/version.ts
+++ src/modules/version.ts
@@ -19,20 +19,24 @@ const color = require('cli-color');
 export function meta()
 {
     return {
-        command: 'version',
-        description: 'バージョン表示',
-        options: [
-            ['-q, --quiet', 'バージョン文字列のみ出力する'],
-        ],
+        command: 'version [options]',
+        describe: 'バージョン表示',
+        options: {
+            'quiet': {
+                alias: 'q',
+                describe: 'バージョン文字列のみ出力します。',
+                type: 'boolean',
+            },
+        },
     }
 }
 
 /**
  * コマンド実行
  */
-export function action(commands:any)
+export function action(argv:any, lampman:any)
 {
-    if(commands.quiet) {
+    if(argv.quiet) {
         process.stdout.write(color.move(0, -1))
         process.stdout.write(libs.getLampmanVersion())
     }  else {
