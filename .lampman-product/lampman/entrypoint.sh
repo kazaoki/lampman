d46ec30844a7c560653983fb0addade245fa6fb2
karino <karino@kazaoki.jp>
2021-01-06 13:00:27 +0900

    [UPDATE] XDebug3系に対応

--- .lampman-product/lampman/entrypoint.sh
+++ .lampman-product/lampman/entrypoint.sh
@@ -35,20 +35,42 @@ if [[ $LAMPMAN_PHP_ERROR_REPORT == 1 ]]; then
 fi
 echo "[Date]" >> $phpini
 echo "date.timezone = \"Asia/Tokyo\"" >> $phpini
-sed -i "/xdebug\.remote_enable/d" $phpini
-sed -i "/xdebug\.remote_autostart/d" $phpini
-sed -i "/xdebug\.remote_host/d" $phpini
-sed -i "/xdebug\.remote_port/d" $phpini
-if [[ $LAMPMAN_PHP_XDEBUG_START == '1' ]]; then
-  echo 'xdebug.remote_enable = On' >> $phpini
-  echo 'xdebug.remote_autostart = On' >> $phpini
-  echo "xdebug.remote_host=$LAMPMAN_PHP_XDEBUG_HOST" >> $phpini
-  if [[ $LAMPMAN_PHP_XDEBUG_PORT != '' ]]; then
-    echo "xdebug.remote_port=$LAMPMAN_PHP_XDEBUG_PORT" >> $phpini
+xdebug_version=$(/root/.anyenv/envs/phpenv/shims/php --ri xdebug | grep -i version)
+# for XDebug 2.x
+if [[ $xdebug_version =~ \ 2\. ]]; then
+  sed -i "/xdebug\.remote_enable/d" $phpini
+  sed -i "/xdebug\.remote_autostart/d" $phpini
+  sed -i "/xdebug\.remote_host/d" $phpini
+  sed -i "/xdebug\.remote_port/d" $phpini
+  if [[ $LAMPMAN_PHP_XDEBUG_START == '1' ]]; then
+    echo 'xdebug.remote_enable = On' >> $phpini
+    echo 'xdebug.remote_autostart = On' >> $phpini
+    echo "xdebug.remote_host=$LAMPMAN_PHP_XDEBUG_HOST" >> $phpini
+    if [[ $LAMPMAN_PHP_XDEBUG_PORT != '' ]]; then
+      echo "xdebug.remote_port=$LAMPMAN_PHP_XDEBUG_PORT" >> $phpini
+    fi
+  else
+    echo 'xdebug.remote_enable = Off' >> $phpini
+    echo 'xdebug.remote_autostart = Off' >> $phpini
   fi
-else
-  echo 'xdebug.remote_enable = Off' >> $phpini
-  echo 'xdebug.remote_autostart = Off' >> $phpini
+fi
+# for XDebug 3.x
+if [[ $xdebug_version =~ \ 3\. ]]; then
+  sed -i "/xdebug\.client_host/d" $phpini
+  sed -i "/xdebug\.client_port/d" $phpini
+  sed -i "/xdebug\.mode/d" $phpini
+  sed -i "/xdebug\.start_with_request/d" $phpini
+  if [[ $LAMPMAN_PHP_XDEBUG_START == '1' ]]; then
+    echo "xdebug.client_host=$LAMPMAN_PHP_XDEBUG_HOST" >> $phpini
+    if [[ $LAMPMAN_PHP_XDEBUG_PORT != '' ]]; then
+      echo "xdebug.client_port=$LAMPMAN_PHP_XDEBUG_PORT" >> $phpini
+    fi
+    echo "xdebug.mode = debug" >> $phpini
+    echo "xdebug.start_with_request = yes" >> $phpini
+  else
+    echo "xdebug.mode = Off" >> $phpini
+    echo "xdebug.start_with_request = default" >> $phpini
+  fi
 fi
 sed -i "s/^variables_order .*$/variables_order = \"EGPCS\"/" $phpini
 echo $phpini > /phpinipath
